a49989f0ac068a48fe692d0c551f47c408cefc08
pkafei <plburton@gmail.com>
2025-08-31 18:59:31 -0400

    Added newletter CTA

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Hero from "../components/Hero";
 import NavBar from "../components/NavBar";
+import HeroNewsletterForm from "../components/HeroNewsletterForm";
 
 export default function Home() {
   return (
     <>
       <NavBar />
+      <HeroNewsletterForm />
       <Hero />
     </>
   );
